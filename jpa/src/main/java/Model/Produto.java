8cd2b0d6c19a410803ef7965dfb0221a3e27b439
LcasQueirxz <161960484+LcasQueirxz@users.noreply.github.com>
2024-11-20 17:05:52 -0300

    first update

---
+++ jpa/src/main/java/Model/Produto.java
@@ -0,0 +1,41 @@
+
+package Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduto;
+    private String nomeProduto;
+    private double precoProduto;
+
+    // Getters e Setters
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+}
